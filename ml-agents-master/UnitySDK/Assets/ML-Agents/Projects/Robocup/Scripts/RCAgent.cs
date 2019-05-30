0d41a32dac7a09daa00883133ac1dbd32f6ea3a6
Lochie Westfall <lochiewestfall@gmail.com>
2019-05-30 10:26:18 +1000

    Add Unity ML Agents and basic project Haven't tested the simple project enough to know how well it works

---
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Projects/Robocup/Scripts/RCAgent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RCAgent : Agent {
+
+    public Transform target;
+
+    public override void CollectObservations()
+    {
+        AddVectorObs(target.position.x);
+        AddVectorObs(transform.position.x);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        float delta = Mathf.Sign(vectorAction[0]*20f - transform.parent.position.x);
+
+        transform.parent.position += Vector3.right * delta * Time.deltaTime * 25f * Mathf.Abs(vectorAction[1]);
+
+        //transform.parent.position = Vector3.right * vectorAction[0] * 20f + Vector3.up;
+
+        SetReward((Vector3.Distance(transform.parent.position, target.position)<1f)?1:0);
+    }
+
+    public override void AgentReset()
+    {
+        transform.parent.position = Vector3.up + Vector3.right*Random.Range(-2f, 2f);
+        target.position = Vector3.up + Vector3.right * Random.Range(-2f, 2f);
+    }
+
+    public override void AgentOnDone()
+    {
+        transform.parent.position = Vector3.up + Vector3.right * Random.Range(-2f, 2f);
+        target.position = Vector3.up + Vector3.right * Random.Range(-2f, 2f);
+    }
+}
