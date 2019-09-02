6aca800a571a7c47c638139b8c80792e30f6010b
Lochie Westfall <lochiewestfall@gmail.com>
2019-09-02 12:46:06 +1000

    Fill AgentController with low level control functions, Add AgentConfig scriptable object

---
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Projects/Robocup/Scripts/AgentConfig.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AgentConfig", menuName = "ScriptableObjects/AgentConfig", order = 1)]
+public class AgentConfig : ScriptableObject
+{
+    [Header("Falling Config")]
+    public bool isFallingEnabled;
+    public float fallRecoveryTime;
+
+    [Header("Kick Config")]
+    public float timeToKick;
+    public float kickPower;
+    public float kickableRange;
+
+    [Header("Walk Config")]
+    public float walkSpeed;
+    public float turnRadius;
+    [HideInInspector]
+    public float turnTime {
+        get {
+            return (turnRadius*2f*Mathf.PI) / walkSpeed;
+        }
+    }
+}
--- ml-agents-master/UnitySDK/Assets/ML-Agents/Projects/Robocup/Scripts/AgentController.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Projects/Robocup/Scripts/AgentController.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class AgentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public AgentConfig config;
+
+    // State variables
+
+    float timeLastCollided;
+    float timeInBallRange;
+    float timeLastKicked;
+
+    // Methods
+
+    public bool isStanding {
+        get { 
+            return Time.time - timeLastCollided > config.fallRecoveryTime;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public bool canKick {
+        get {
+            bool hasKickDelayExpired = timeInBallRange > config.timeToKick;
+            bool isWithinKickRange = config.kickableRange < Vector3.Distance(transform.position, GameObject.Find("ball").transform.position);
+            return timeInBallRange > config.timeToKick;
+        }
+    }
+
+    bool KickBallAtAngle (float angle) {
+        if (isStanding && canKick) {
+            Vector3 kickVector = Quaternion.AngleAxis(angle, Vector3.up) * transform.forward;
+            kickVector.Normalize();
+            GameObject.Find("ball").GetComponent<Rigidbody>().AddForce(kickVector * config.kickPower);
+            timeLastKicked = Time.time;
+            timeInBallRange = 0f;
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool Walk (float direction) {
+        if (isStanding) {
+            GetComponent<Rigidbody>().angularVelocity = (new Vector3(0f, Time.deltaTime * 360f / config.turnTime * direction, 0f));
+            GetComponent<Rigidbody>().velocity = transform.forward * config.walkSpeed;
+
+            return true;
+        } 
+        else {
+            return false;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (config.isFallingEnabled && (other.gameObject.tag == "left" || other.gameObject.tag == "right")) {
+            timeLastCollided = Time.time;
+            Vector3 collisionReactionVector = transform.position - other.transform.position;
+            collisionReactionVector.y = 0;
+            transform.position += (collisionReactionVector).normalized * 0.125f;
+            timeInBallRange = -config.fallRecoveryTime;
+        }
     }
 }
